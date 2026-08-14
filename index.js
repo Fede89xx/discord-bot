fd98b201cf487cd4c95e489ffa85c0ef562d4e63
fxd!x <federicomartintorres7@gmail.com>
2026-08-14 20:32:01 -0300

    Change welcome channel ID in index.js
    Updated the channel ID for welcoming new members.

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ client.once("ready", () => {
 
 client.on("guildMemberAdd", async (member) => {
 
-    const canal = member.guild.channels.cache.get("1533052976988688476");
+    const canal = member.guild.channels.cache.get("1537966672839970836");
 
     if (!canal) return;
 
